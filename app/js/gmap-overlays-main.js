61eeda9984b62bee5d7039506fe22a5442075fcb
philip tutty <ptutty@gmail.com>
2012-11-16 14:14:22 +1000

    added google maps and key

--- app/js/gmap-overlays-main.js
+++ app/js/gmap-overlays-main.js
@@ -15,7 +15,7 @@ function createOverlays(map) {
             strokeOpacity: 0.35,
             strokeWeight: 1,
             fillColor: color,
-            fillOpacity: 0.55
+            fillOpacity: 0.65
           });
           poly.setMap(map);
   }
@@ -28,54 +28,12 @@ function createOverlays(map) {
     }      
 } 
 
-
-// the polygon data
-
-// array of polygon paths
-var redpaths = [[
-new google.maps.LatLng(-27.537741,152.973976),
-new google.maps.LatLng(-27.540025,152.975006),
-new google.maps.LatLng(-27.534545,152.991657),
-new google.maps.LatLng(-27.522062,152.991828),
-new google.maps.LatLng(-27.515212,152.984447),
-new google.maps.LatLng(-27.507142,152.975006),
-new google.maps.LatLng(-27.506381,152.971744),
-new google.maps.LatLng(-27.509426,152.968826),
-new google.maps.LatLng(-27.512623,152.967796),
-new google.maps.LatLng(-27.517343,152.968311),
-new google.maps.LatLng(-27.526629,152.972774),
-new google.maps.LatLng(-27.531957,152.971744),
-new google.maps.LatLng(-27.534088,152.971401),
-new google.maps.LatLng(-27.537741,152.973976)
-],
-[
-new google.maps.LatLng(-27.504554,152.972602),
-new google.maps.LatLng(-27.510796,152.983589),
-new google.maps.LatLng(-27.512471,152.986335),
-new google.maps.LatLng(-27.500747,152.993545),
-new google.maps.LatLng(-27.491459,152.997150),
-new google.maps.LatLng(-27.489936,152.994575),
-new google.maps.LatLng(-27.486738,152.994575),
-new google.maps.LatLng(-27.478210,153.000240),
-new google.maps.LatLng(-27.469986,153.009510),
-new google.maps.LatLng(-27.466330,153.007621),
-new google.maps.LatLng(-27.465721,153.002471),
-new google.maps.LatLng(-27.465112,152.996292),
-new google.maps.LatLng(-27.474707,152.986164),
-new google.maps.LatLng(-27.494504,152.973632),
-new google.maps.LatLng(-27.504554,152.972602)
-]];
-
-var greenpaths = [[new google.maps.LatLng(-27.511558,152.984619),
-new google.maps.LatLng(-27.510796,152.998523),
-new google.maps.LatLng(-27.504097,153.005905),
-new google.maps.LatLng(-27.492372,153.006248),
-new google.maps.LatLng(-27.491611,152.997493),
-new google.maps.LatLng(-27.490545,152.995433),
-new google.maps.LatLng(-27.488109,152.994232),
-new google.maps.LatLng(-27.496941,152.983589),
-new google.maps.LatLng(-27.508969,152.981185),
-new google.maps.LatLng(-27.511558,152.984619)]]
-
 // array of path data objects
-var pathdata = [{"color" : "#FF0000", "data": redpaths}, { "color" : "#CCEDBA", "data": greenpaths}];
+var pathdata = [
+{"color" : "#2277ff", "data": bluepaths},
+{"color" : "#116633", "data": greenpaths}, 
+{"color" : "#7700bb", "data": purplepaths},
+{"color" : "#dd0000", "data": redpaths},
+{"color" : "#eeff55", "data": yellowpaths},
+
+ ]
