3353530cee7aefd31a525bd9b46fb9df37b0e4cd
philip tutty <ptutty@gmail.com>
2012-11-20 16:55:18 +1000

    added new service for general cross controller communication

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,35 +2,44 @@
 
 /* Controllers */
 
-
-
-function MainMap($scope, Suburb, querySuburb){
- $scope.suburbs = Suburb.query();
- $scope.query = querySuburb.query;
+function HeaderCtrl($scope, appServices){
+ $scope.currentviews = appServices.currentviews;
+ $scope.suburbdetail = appServices.suburbdetail; 
 }
 
-function SuburbListCtrl($scope, Suburb, querySuburb) {	
+function MainMap($scope, Suburb, appServices){
+ $scope.suburbs = Suburb.query();
+ $scope.query = appServices.query;
+ appServices.currentviews.suburbdetail = false;
+}
+
+function SuburbListCtrl($scope, Suburb, appServices) {	
   $scope.suburbs = Suburb.query();
   $scope.orderProp = 'name';
   $scope.message = false;
-  $scope.query = querySuburb.query; 
+  $scope.query = appServices.query; 
 
-  if (querySuburb.query.time != 0) {
-      $scope.message = true;
-    } else {
-      $scope.message = false;
-    } 
+  $scope.resettime = function(){
+    appServices.query.time = 0;
+  }
+  $scope.resetname = function(){
+    appServices.query.name = "";
+  }
 }
 
 
-function SuburbDetailCtrl($scope, $routeParams, Suburb) {
+function SuburbDetailCtrl($scope, $routeParams, Suburb, appServices) {
   $scope.suburb = Suburb.get({suburbId: $routeParams.suburbId}, function(suburb) {
     $scope.mainImageUrl = suburb.images[0];
+    appServices.suburbdetail.current = suburb.name;
   });
 
   $scope.setImage = function(imageUrl) {
     $scope.mainImageUrl = imageUrl;
   }
+  // update appService info - for cross controller sharing of state
+  appServices.currentviews.suburbdetail = true;
+  
 }
 
 
--- app/js/services.js
+++ app/js/services.js
@@ -11,11 +11,29 @@ angular.module('suburbcatServices', ['ngResource']).
 
 
 // Service for cross controller communication of search query
-App.factory('querySuburb', function() {  
+/* App.factory('querySuburb', function() {  
     return {
         query : {
             name : "",
             time : 0,
+        }
+    };
+}); */
+
+// service for cross controller communication
+App.factory('appServices', function() {  
+    return {
+        query : { // for suburb search filter sharing between controllers 
+            name : "",
+            time : 0,
+        },
+        currentviews: { // for current view state sharing between controllers
+           mainmap: true,
+           suburbdetail: false,
+           suburblisting: true      
+        },
+        suburbdetail: { // for current view state sharing between controllers
+           current: ""     
         }
     };
 });
