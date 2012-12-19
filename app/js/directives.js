879ec384d2289f1bf853eb94dc8a8bf2f51ce5a7
philip tutty <ptutty@gmail.com>
2012-12-19 14:30:58 +1000

    refactoring maps 50% done

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,51 +4,82 @@
 var subcatdirectives = angular.module('suburbcatDirectives', []);
 
 
-// main gmap
-subcatdirectives.directive('gmapmain', function($parse, MapOverlays) {
+// google map on homepage
+subcatdirectives.directive('suburbsmap', function($parse) {
 
-    return {
-        restrict: 'E',
-        replace: true,      
-        template: '<div></div>',     
-        link: function(scope, element, attrs) { 
-		    var myOptions = {
-				            zoom: 12,
-				            center: new google.maps.LatLng(-27.5222, 153.0031),
-				            mapTypeId: google.maps.MapTypeId.ROADMAP
+	var suburbMarkerArray = []; 
+	var suburbsMap; // google map object
+
+	function mapCreate(id) {
+		var myOptions = {
+			            zoom: 12,
+			            center: new google.maps.LatLng(-27.5222, 153.0031),
+			            mapTypeId: google.maps.MapTypeId.ROADMAP
 				        };	
-		    var map = new google.maps.Map(document.getElementById(attrs.id), myOptions);  
+		suburbsMap = new google.maps.Map(document.getElementById(id), myOptions); 
+	};
+
+	function mapCreateMarker(suburbMarkerData, infoWindowText){
+		var latLng = new google.maps.LatLng(suburbMarkerData.lat, suburbMarkerData.lng);
+		var marker = new google.maps.Marker({
+		  position: latLng,
+		  map: suburbsMap,
+		  icon: suburbMarkerData.icon,
+		  html: infoWindowText
+		});
+		return marker;
+	};
+
+	function mapCreateInfoWindow(marker){
+		var infowindow = new google.maps.InfoWindow({
+		content: "holding..."
+		});
+		google.maps.event.addListener(marker, 'click', function() {
+			infowindow.setContent(marker.html);
+			infowindow.open(suburbsMap, marker);
+		}); 
+		return infowindow; 
+	};
+
+	function mapInitialize(id){
+		mapCreate(id);
+		var stluciaCampusMapData = {"lat": -27.4978, "lng": 153.0132, "icon": "img/university.png"};
+		var infoWindowText = "<h3>University of Queensland</h3><p>St Lucia Campus</p>";
+		var stluciaMarker = mapCreateMarker(stluciaCampusMapData, infoWindowText);
+		mapCreateInfoWindow(stluciaMarker);
+	};
+
+	function suburbsMarkersInitialize(suburbsData){
+        for (var i = 0; i < suburbsData.length; i++) {
+          var suburb = suburbsData[i];
+          var suburbMarker = mapCreateMarker(suburb.gmap.marker);
+          var infoWindowText = "<h3><a href='#/suburbs/" + suburb.id + "'>"+ suburb.name +"</a></h3><p>Travel time St Lucia: " + stlucia + "</br>Travel time Herston: " + herston + "</p></a>"
+          mapCreateInfoWindow(suburbMarker);
+      	}
+	}
 
 
-		     // show marker for st lucia campus
-	        var image = "img/university.png";
-	        var campusLatLng = new google.maps.LatLng(-27.4978,153.0132);
-	        var campusMarker = new google.maps.Marker({
-	          position: campusLatLng,
-	          map: map,
-	          icon: image
-	        });
-	        // setup default infowindo
-	        var infowindow = null;
-			var infowindow = new google.maps.InfoWindow({
-				content: "holding..."
+	return {
+		restrict: 'E',
+		replace: true,      
+		template: '<div></div>',     
+		link: function(scope, element, attrs) {	
+			mapInitialize(attrs.id);
+	 		
+			attrs.$observe('suburbsdata', function(data) {
+				var data = angular.fromJson(data);
+				if (data.length !== 0) {
+					suburbsMarkersInitialize(data);
+				};
+					
+			
+				// MapOverlays.manMarkers(map, suburbs, infowindow);
 			});
-			google.maps.event.addListener(campusMarker, 'click', function() {
-              // window.location = "#/suburbs/" + suburb.id;
-              infowindow.setContent("<h3>University of Queensland</h3> <h4>St Lucia campus</h4>");
-              infowindow.open(map, campusMarker);
-      		}); 
-      		campusMarker.setMap(map);
 
-
-		    attrs.$observe('gdata', function(data) {
-				var suburbs = angular.fromJson(data);	
-				MapOverlays.manMarkers(map, suburbs, infowindow);
-			});
-
-		    attrs.$observe('distance', function(distance) { 
-				MapOverlays.manPolys(map, distance);
-		    });
-        }
-    }
+		/*
+		attrs.$observe('distance', function(distance) { 
+			MapOverlays.manPolys(map, distance);
+		});*/
+		}
+	}
 })
